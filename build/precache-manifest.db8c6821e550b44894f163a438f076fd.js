ae1cbebf31913ecd7f16d259732a349481e9d9ea
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-03-25 17:58:43 +0530

    First react project

---
+++ build/precache-manifest.db8c6821e550b44894f163a438f076fd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b9405c6e19595afd920080abfbdce8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b621e135df34e2aa394",
+    "url": "/static/js/2.46c8e90e.chunk.js"
+  },
+  {
+    "revision": "924b25331b5e1d9854c4",
+    "url": "/static/js/main.8a528acc.chunk.js"
+  },
+  {
+    "revision": "325f4b8f32369198a2c5",
+    "url": "/static/js/runtime-main.4e550677.js"
+  }
+]);
