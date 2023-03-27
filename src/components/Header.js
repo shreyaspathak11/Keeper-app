39a1a1ddf75e9cbb639753dafe369f3c669b5a31
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-03-27 23:57:54 +0530

    version 2 added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 function Header() {
-    return (
+  return (
     <header>
-        <h1>Keeper</h1>
+      <h1>Keeper</h1>
     </header>
-    );
+  );
 }
+
 export default Header;
